cdb0deccdbbad7a973927580e70ab2e6cca83840
Mehdi Bouassami <mbouassa@ucsd.edu>
2022-09-27 20:56:08 -0700

    Updates

---
+++ precache-manifest.d0c2b09b0e447d9ec656857c2bebb8a2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "103cf8065b57f7e2f770",
+    "url": "/MyVacae_Website/static/css/main.2c956e7a.chunk.css"
+  },
+  {
+    "revision": "103cf8065b57f7e2f770",
+    "url": "/MyVacae_Website/static/js/main.103cf806.chunk.js"
+  },
+  {
+    "revision": "0d75e45961ae93bb12bd",
+    "url": "/MyVacae_Website/static/css/1.9583a7e5.chunk.css"
+  },
+  {
+    "revision": "0d75e45961ae93bb12bd",
+    "url": "/MyVacae_Website/static/js/1.0d75e459.chunk.js"
+  },
+  {
+    "revision": "e81ad094474f7a6b1144",
+    "url": "/MyVacae_Website/static/js/runtime~main.e81ad094.js"
+  },
+  {
+    "revision": "d2e373e11cb629d4f5a4015cf8de1b72",
+    "url": "/MyVacae_Website/static/media/Travl.d2e373e1.png"
+  },
+  {
+    "revision": "8015ec7b2e6aec7af70a55c4926fff3b",
+    "url": "/MyVacae_Website/static/media/Hotel_mv.8015ec7b.png"
+  },
+  {
+    "revision": "d6664c81ae8ccfcff6a98767e91d3bb0",
+    "url": "/MyVacae_Website/static/media/Airbnb_mv.d6664c81.png"
+  },
+  {
+    "revision": "ea2de46eef2fb066e1d3e306f04bbd79",
+    "url": "/MyVacae_Website/static/media/inquiry_mv.ea2de46e.png"
+  },
+  {
+    "revision": "9e2ce092eb1fc066175c9b822e44c14d",
+    "url": "/MyVacae_Website/static/media/Select_mv.9e2ce092.png"
+  },
+  {
+    "revision": "cf4f57d0ebcf93dac6d32fc41d90501e",
+    "url": "/MyVacae_Website/static/media/dub_mv.cf4f57d0.png"
+  },
+  {
+    "revision": "77c9b5a3c902dd03b7f14ce5a643a99e",
+    "url": "/MyVacae_Website/static/media/hot_1_mv.77c9b5a3.jpg"
+  },
+  {
+    "revision": "025655b4e7d2741309cfe2b81e28c596",
+    "url": "/MyVacae_Website/static/media/hot_2_mv.025655b4.jpg"
+  },
+  {
+    "revision": "2678d0408ac03f73c02f2f0ee9d25c7b",
+    "url": "/MyVacae_Website/static/media/hot_3_mv.2678d040.jpg"
+  },
+  {
+    "revision": "60bc18b595fee32d57e1f2d0dbaac2b6",
+    "url": "/MyVacae_Website/static/media/calendar_MyPlan.60bc18b5.jpg"
+  },
+  {
+    "revision": "246018ab2488ec1779b43c99b4a84cd7",
+    "url": "/MyVacae_Website/static/media/graph_MyPlan.246018ab.jpg"
+  },
+  {
+    "revision": "c0d60765916a843eabc54426dc7138e6",
+    "url": "/MyVacae_Website/static/media/questions_sleep.c0d60765.jpg"
+  },
+  {
+    "revision": "a8dc27c6c9cb7873b97e60ef088eb792",
+    "url": "/MyVacae_Website/static/media/Add_MyPlan.a8dc27c6.jpg"
+  },
+  {
+    "revision": "57e6ada610a0001c110434c61fdf0d88",
+    "url": "/MyVacae_Website/static/media/sport_MyPlan.57e6ada6.jpg"
+  },
+  {
+    "revision": "9f45c37c2881153cd8c1fc39833c7505",
+    "url": "/MyVacae_Website/static/media/prod_MyPlan.9f45c37c.jpg"
+  },
+  {
+    "revision": "328cff52adef59802c03785cad58c228",
+    "url": "/MyVacae_Website/static/media/Sleep_MyPlan.328cff52.jpg"
+  },
+  {
+    "revision": "6029613d27ea29d36eec386f90f93240",
+    "url": "/MyVacae_Website/index.html"
+  }
+];
